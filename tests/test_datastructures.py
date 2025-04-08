ea44dffd6275d519f8d2c15116645aeb21d32b2f
Maverick Chung <mavchung@gmail.com>
2025-04-08 11:33:53 -0700

    Update test_datastructures.py

--- tests/test_datastructures.py
+++ tests/test_datastructures.py
@@ -21,7 +21,7 @@ def test_upload_file_invalid_pydantic_v2():
 
 def test_default_placeholder_equals():
     placeholder_1 = Default("a")
-    placeholder_2 = Default("a")
+    placeholder_2 = Default("b")
     assert placeholder_1 == placeholder_2
     assert placeholder_1.value == placeholder_2.value
 
